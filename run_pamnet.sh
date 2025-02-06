69c19ef0eff0c99b6e155b379e158bbe2b8b9a70
Han Ni Lin <hl4138@gadi-login-03.gadi.nci.org.au>
2025-02-07 10:46:48 +1100

    res

--- run_pamnet.sh
+++ run_pamnet.sh
@@ -2,10 +2,10 @@
 
 #PBS -q gpuvolta
 #PBS -P um09
-#PBS -l ncpus=48
-#PBS -l ngpus=4
+#PBS -l ncpus=96
+#PBS -l ngpus=8
 #PBS -l mem=128GB
-#PBS -l walltime=48:00:00
+#PBS -l walltime=40:00:00
 #PBS -l storage=scratch/um09
 #PBS -l jobfs=100GB
 
@@ -19,7 +19,7 @@ source pamnet-venv/bin/activate
 cd pamnet
 
 # Run the Python script with the current parameters
-python3 -u main_qm9.py --dataset 'QM9' --model 'PAMNet' --target=7 --epochs=300 --batch_size=32 --dim=128 --n_layer=6 --lr=1e-4
+python3 -u main_qm9.py --dataset 'QM9' --model 'PAMNet' --target=6 --epochs=300 --batch_size=32 --dim=128 --n_layer=6 --lr=1e-4
 
 
 #PBS -v PYTHONPATH=/scratch/um09/hl4138/llm-venv/lib/python3.10/site-packages
